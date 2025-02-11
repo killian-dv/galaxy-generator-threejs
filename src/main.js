ec26846bb28a0abde85b01b0259aaeb099b4a7da
killian-dv <killian.david@etudiant.univ-reims.fr>
2025-02-11 19:38:09 +0100

    ✨ : add roation animation

--- src/main.js
+++ src/main.js
@@ -217,6 +217,9 @@ const clock = new THREE.Clock();
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
+  // rotate the galaxy
+  points.rotation.y = elapsedTime * 0.05;
+
   // Update controls
   controls.update();
 
